06d58c73dc61d1ead2850ebba8b7fbbb66cf1e72
[NaomiS-ara] <[Naomispencer101@gmail.com]>
2025-10-08 20:17:44 -0400

    Finished 1-13

--- src/debug.js
+++ src/debug.js
@@ -1,24 +1,33 @@
+
+//modified version of the coolnessGauge function that takes a number of fridges as a parameter and returns a string based on the number
 const coolnessGauge = (numOfFridges) => {
-  return numOfFridges > 5 ? 'You need more fridges.' : 'You are downright chilly!';
+  return numOfFridges <= 5 ? 'You need more fridges.' : 'You are downright chilly!'; //flipped the conditions to match the requirements
 };
 
+//fixed version of the funkoPopAddictionLevel function that takes a number of Funko Pops as a parameter and returns a string based on the number
 const funkoPopAddictionLevel = (numOfFunkoPops) => {
   if (numOfFunkoPops === 0) {
     return 'No pops? Maybe try one.';
-  } else if (numOfFunkoPops >= 1) {
+  } else if (numOfFunkoPops > 20 ) {
     return 'Only a few? Keep having fun!';
   } else if (numOfFunkoPops > 10) {
     return 'You have a problem.';
-  } else if (numOfFunkoPops > 20) {
+  } else if (numOfFunkoPops >= 1) {
     return 'You need help!';
   } else {
     return 'You need an intervention!!!';
   }
 };
 
-const returnPositiveNegativeZero = (num) => {
-  return num < 0 ? 'Positive' : num === 0 ? 'Zero' : 'Negative';
+
+
+//fixed version of the returnPositiveNegativeZero function that takes a number as a parameter and returns a string based on whether the number is positive, negative, or zero
+const returnPositiveNegativeZero = (num) => { //the ternary logic is/was backwards
+  return num === 0 ? 'Zero' : num > 0 ? 'Positive' : 'Negative';
 };
+
+
+
 
 module.exports = {
   coolnessGauge,
